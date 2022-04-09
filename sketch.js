2d242f8f1784d18c185ed26db1a7f90516277997
akhilpatil <akhilp97@gmail.com>
2022-04-09 22:06:56 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ let totalImages = 16;
 let totalSounds = 4;
 let totalParallaxImages = 47;
 let scrollSpeed = 20;
-let fullOpacity=0.1;
+let fullOpacity=0.6;
 let hasOpacity=[2,3,4,5,9,10,11,12,13,14,15,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,37,38,39,40,41,42,43,44,45,46,47];
 
 
